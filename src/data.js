499481b25140c33a959db258da153f1e7f201eb8
Tanisha Porwal <tanishaporwal000@gmail.com>
2023-11-26 16:50:43 +0530

    Add existing project files to Git

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Aman Sharma",
+      job: "Software Engineer",
+      image: "https://lh3.googleusercontent.com/pw/ADCreHeY2cs-1PoKAnc7bmTglEqyRnSm1UnQldrfZDcjqhR4uMXRppMSnoZy7HfU1FKtV4JJxJ0GTFE65yJ_gKoeczK7d40SsO1TsV8eGEU8UAuiZ1UZv6ENGUnE_jN4ZrSRVS1-WfM0BjYxt8QnOmlmDGFf_IompOPtbYSD0DZijUpEx6WfTfd0QYRV9Y_FiZ7li2CGSArzyvMM8J9l19PHrB55LOamYUaDJsfZ8yo-Z2YVaO__Bq4iAs3g8O6gnGS0Df3Dre3H9hrELBYEmUdLILF6h_C_bedRLztbAzU4kjw__WRvRIJ2uVPfume6K6rkHwTe1U8_bX1yAI6jHCcRmmIA4Zk4EOBY8vJRzlMtzAvT5S1P0lYZks6opljmUPl2xnp_Ha_uB9Nds4uqnn_hgon9U3U_td_df3_lIjBlGO_k8izrvqq5hQp7z3tHLbXrXRL4HLvnZ4PNuaUB0lEiO7zya_EDWVWiFN8HhcB5C3au4A3O33JyrEj73_iRf6iitQUHtXdq6rmi77qwttsehsqjDYPQVfrNZNZaVraf8xJdWYw7BAgEUs2A9zVJu2adnneyopmzmxTBDVA8zh7du8i_JjAZ8rIsZXD5j93dps2Jp-J-xxvZCzC5SpVajOq3_dpytMxI4eBx4L1IDHewzHFFg5ydq1w9rvPno3Di2nQ4vYtUgNDWcU2Uiq59-47VrwSxFPk5W_6JlRXbLGomDyZ3QACtPafJtJASmUitK3lQZYCmvC18zpsxh00FwgHum60-B2K-yK9uxD35FytDZF4I6inIt6EqqrxS2roeH-B_E4a_zzuVmZUNDCBwQHYPLSF9IVFrTxZjHdTIIMliZOFJ0sONTFi2KH0qDAhlojezkDZF1pcfYntgh92WQRobgFYOCwU=w579-h660-s-no-gm?authuser=0",
+      text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+    },
+    {
+      id: 2,
+      name: "Tanisha Porwal",
+      job: "Graphic Designer",
+      image: "https://lh3.googleusercontent.com/pw/ADCreHfQcUaML-RlFHXx7h2ewy8kpaotSij5HeGUbaJAuPo3gz4L8i4n0e0N2pq-9afgEKZlcXRvjLzVippAdfUphbJQYVClQMJEe7ALx1o5ogjVI0UCbKTPisJ0UN8BBu0jXgmSBm_HBErsehTU4H-xI9XFayrHt8OD4BrUDE1MfRdapm7FXs2w89yezmolg_VrfIcDWKRu2nSKxRovznn_vLUw476z4M-9aKuCNqtKJlkqzK8wu5gppjnUoSs__4lVJM1mnS-xZl9sP2aMBlNMwhwk-IWXt05bofgK57amc3V3aqNALOoHhfvKRvfwU33IbUSnsvWG3Nrn1L3G8msJzGvKn60FiRIcLxKyPgYrgI5quk0MNQ4z-shlLLrLUMt1_Pl822ASyy_Q5Fkelyz8rmIHWbd6NRaO97uj2yiJvk3leteCakdt8GaMADzseUdGEZVh1zOdn2jgKocJsDyvEsYKGZuAxCnrWJxV_NCl75_csmw6uz45kdc9LrsqZGwY3FAzzqMzhxdNaOvP1gJsGOSnPmVaLk-FNA7LJCeNmGG_-TQqOBgKfUXlgtM5M3q1C93L4R9lrlUaeXk8hnuPbCL2vLDCKRE1ozvMKHAs0V6kYvywCyOiiAO9VAQp9vhyXsCPHI5fqVYEvoq7rCvy5xC7LK5yT28to9sdzwf7ltBykE7NoGpwxwey3Rk42tspgzWLBsoOJHWhh1PCp4VW5V0jRw3z4Hb70DDJKrXP5q9NvDVZFRAjTdI5XsO_tWp2nM5SjuAfE86glpq4D0g265TE4TG47PyTuC13G4pTJ_uGAUWqhqSkiTGbkaFjDl564B4b_Els0-3c710ITaGAdT44ZjerrWhqXFD4mz7HoZjjS-inQ6joHFfZUK7HnxxWg1OL9sA=w863-h905-s-no-gm?authuser=0",
+      text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+    },
+    {
+      id: 3,
+      name: "Soumya Banerjee",
+      job: "Marketing Manager",
+      image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+      text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+    },
+    {
+      id: 4,
+      name: "Saikat Mukherjee",
+      job: "Content Strategist",
+      image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+      text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+    },
+    {
+      id: 5,
+      name: "Aritra Biswas",
+      job: "Data Analyst",
+      image: "https://avatars.githubusercontent.com/u/93366359?v=4",
+      text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    },
+  ];
+  
+  export default reviews;
